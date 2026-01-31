37260d56c00ff52a89a2596c9bf00a4238aa072a
Zakir Mansurov <zakir_mansurov@bk.ru>
2026-01-31 19:17:09 +0500

    Fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // Игнорируем ошибки типов (те самые 22 ошибки)
-    ignoreBuildErrors: true,
-  },
-  
+  /* config options here */
 };
 
 export default nextConfig;
