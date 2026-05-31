e91307ca9ee9bfcaf54281ba787995da48b7a379
Zakir Mansurov <zakir_mansurov@bk.ru>
2026-05-31 13:57:50 +0500

    fix: SVG wave stretching on wide screens

--- app/page.tsx
+++ app/page.tsx
@@ -143,7 +143,7 @@ export default function HomePage() {
 
       {/* WAVE SEPARATOR */}
       <div className="relative -mt-24 h-24 overflow-hidden">
-        <svg viewBox="0 0 1440 320" className="absolute bottom-0 w-full h-full text-[#F2F6FF] fill-current preserve-3d">
+        <svg viewBox="0 0 1440 320" preserveAspectRatio="none" className="absolute bottom-0 w-full h-full text-[#F2F6FF] fill-current">
           <path fillOpacity="1" d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,250.7C960,235,1056,181,1152,165.3C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>
       </div>
